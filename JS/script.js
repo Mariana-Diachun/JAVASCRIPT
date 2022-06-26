eb43d1658528ef5e34255044aa938a50338ace53
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-06-26 21:23:55 +0200

    add js file

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+
+function getSubstring(string, length) {
+  const string = "Repair droid";
+  const length = 5;
+  const substring = slice(string, length) ; // Change this line
+
+  return substring;
+}
+
+console.log(getSubstring);
