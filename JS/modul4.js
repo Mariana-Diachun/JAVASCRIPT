4fb25cc9967fd56935bfe4044d420e2cdc4c3469
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-07-16 09:28:07 +0200

    add modul 4

---
+++ JS/modul4.js
@@ -0,0 +1,147 @@
+// // Change code below this line
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+
+//   orderedItems.forEach(function (item) {
+//     totalPrice += item;
+//   });
+
+//   return totalPrice;
+// }
+// // Change code above this line
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]))
+
+
+
+
+
+// const calculateTotalPrice = (orderedItems) => {
+//     let totalPrice = 0;
+//    orderedItems.forEach((item) => {
+//     totalPrice += item;
+//   });
+//   return totalPrice
+// }
+
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]))
+
+
+
+
+
+// function changeEven(numbers, value) {
+//   // Change code below this line
+//   const newNumbers = []
+// numbers.forEach((number) => {
+// if (number % 2 === 0) {
+//       numbers = number + value;
+//       newNumbers.push(numbers)
+//     }})
+//   return newNumbers
+//   // Change code above this line
+// }
+
+// console.log(changeEven([2, 8, 3, 7, 4, 6], 10))
+
+
+// // Change code below this line
+// const getUsersWithEyeColor = (users, color) => {
+//   const newArray = []
+//  users.filter(user => user.eyeColor === color) {
+//    newArray.push(user.eyeColor)
+//  }
+// return newArray
+// };
+// // Change code above this line
+
+// console.log()
+
+const users =
+[
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+    age: 37
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+    age: 34
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+    age: 24
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+    age: 21
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+    age: 27
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+    age: 38
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+    age: 39
+  }
+]
+
+// // Change code below this line
+// const getUsersWithFriend = (users, friendName) => {
+//    return users.filter(user => user.friends.incledes(friendName))
+// };
+const getActiveUsers = (users) => {
+    users.filter(user => user.isActiv === true) 
+        return users.filter(user => user.name)
+    }
+   
+;
+// Change code above this line
+
+// console.log(users.filter(user => user.name))
+
+
